edd1dc898f495573a60d577c6b2fa443f7b692ed
AnerVillanueva <xxanerxx46@gmail.com>
2022-12-23 00:27:39 +0100

    first commit

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CV from '../../assets/8.Aner_Villanueva Garcia_CV.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
